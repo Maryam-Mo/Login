35f9c34d6499474b8dfb578529cb350ab0bbae8f
Maryam <maryam_moein@hotmail.com>
2019-10-11 19:23:22 +0800

    implement search on listview

--- app/src/main/java/com/example/maryam/log_in/user/view/CustomListAdapter.java
+++ app/src/main/java/com/example/maryam/log_in/user/view/CustomListAdapter.java
@@ -7,23 +7,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import com.example.maryam.log_in.R;
 import com.example.maryam.log_in.dto.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by maryam on 8/23/19.
  */
 
-public class CustomListAdapter extends ArrayAdapter {
+public class CustomListAdapter extends ArrayAdapter implements Filterable {
     private final Activity context;
     List<User> usersList;
     private TextView usernameFieldTable;
     private TextView firstNameFieldTable;
     private TextView lastNameFieldTable;
+    List<String> arrayList;
+    List<String> mOriginalValues;
 
     public CustomListAdapter(Activity context, List<User> userList) {
         super(context, R.layout.listview_row, userList);
@@ -45,4 +50,54 @@ public class CustomListAdapter extends ArrayAdapter {
         lastNameFieldTable.setText(user.getLastName());
         return rowView;
     }
+
+//    @Override
+//    public Filter getFilter() {
+//        Filter filter = new Filter() {
+//
+//            @SuppressWarnings("unchecked")
+//            @Override
+//            protected void publishResults(CharSequence constraint,FilterResults results) {
+//
+//                arrayList = (List<String>) results.values; // has the filtered values
+//                notifyDataSetChanged();  // notifies the data with new filtered values
+//            }
+//
+//            @Override
+//            protected FilterResults performFiltering(CharSequence constraint) {
+//                FilterResults results = new FilterResults();        // Holds the results of a filtering operation in values
+//                List<String> FilteredArrList = new ArrayList<String>();
+//
+//                if (mOriginalValues == null) {
+//                    mOriginalValues = new ArrayList<String>(arrayList); // saves the original data in mOriginalValues
+//                }
+//
+//                /********
+//                 *
+//                 *  If constraint(CharSequence that is received) is null returns the mOriginalValues(Original) values
+//                 *  else does the Filtering and returns FilteredArrList(Filtered)
+//                 *
+//                 ********/
+//                if (constraint == null || constraint.length() == 0) {
+//
+//                    // set the Original result to return
+//                    results.count = mOriginalValues.size();
+//                    results.values = mOriginalValues;
+//                } else {
+//                    constraint = constraint.toString().toLowerCase();
+//                    for (int i = 0; i < mOriginalValues.size(); i++) {
+//                        String data = mOriginalValues.get(i);
+//                        if (data.toLowerCase().contains(constraint.toString())) {
+//                            FilteredArrList.add(data);
+//                        }
+//                    }
+//                    // set the Filtered result to return
+//                    results.count = FilteredArrList.size();
+//                    results.values = FilteredArrList;
+//                }
+//                return results;
+//            }
+//        };
+//        return filter;
+//    }
 }
--- app/src/main/java/com/example/maryam/log_in/user/view/ViewUsersActivity.java
+++ app/src/main/java/com/example/maryam/log_in/user/view/ViewUsersActivity.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -16,6 +19,7 @@ import com.example.maryam.log_in.user.OnFindAllUsersAndLoginUserListener;
 import com.example.maryam.log_in.user.presenter.UserPresenter;
 import com.example.maryam.log_in.user.presenter.UserPresenterImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Nonnull;
@@ -23,8 +27,11 @@ import javax.annotation.Nonnull;
 
 public class ViewUsersActivity extends AppCompatActivity implements UserView {
     private ListView userList;
+    private EditText filter;
     private String loginUser;
     private UserPresenter userPresenter;
+    private CustomListAdapter customListAdapter;
+    private List<User> userLists;
 
     @SuppressLint("RestrictedApi")
     @Override
@@ -32,13 +39,15 @@ public class ViewUsersActivity extends AppCompatActivity implements UserView {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_users);
         userList = findViewById(R.id.userList);
+        filter = findViewById(R.id.searchFilter);
         userPresenter = new UserPresenterImpl(this);
         userPresenter.findAllUsers(new OnFindAllUsersAndLoginUserListener() {
 
             @Override
             public void onSuccess(@NonNull List<User> userList, @Nonnull String currentUser) {
                 loginUser = currentUser;
-                initializingUserList(userList);
+                userLists = userList;
+                initializingUserList(userLists);
             }
 
             @Override
@@ -62,9 +71,28 @@ public class ViewUsersActivity extends AppCompatActivity implements UserView {
     }
 
     private void initializingUserList(final List<User> users) {
-        CustomListAdapter customListAdapter = new CustomListAdapter(ViewUsersActivity.this, users);
-        userList = findViewById(R.id.userList);
-        userList.setAdapter(customListAdapter);
+        initList(users);
+        filter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().equalsIgnoreCase("")){
+                    initList(userLists);
+                } else {
+                    searchItem(charSequence.toString());
+                }
+//               customListAdapter.getFilter().filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
         userList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
@@ -78,6 +106,23 @@ public class ViewUsersActivity extends AppCompatActivity implements UserView {
                 }
             }
         });
+    }
+
+    private void searchItem(String s) {
+        List<User> users = new ArrayList<User>();
+        for (int index=0; index<userLists.size(); index++) {
+            if (userLists.get(index).getUsername().contains(s)) {
+                users.add(userLists.get(index));
+            }
+        }
+        initList(users);
+    }
+
+    private void initList(List<User> users) {
+        customListAdapter = new CustomListAdapter(ViewUsersActivity.this, users);
+        userList = findViewById(R.id.userList);
+        userList.setAdapter(customListAdapter);
+        userList.setTextFilterEnabled(true);
     }
 
     @Override
